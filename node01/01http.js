9d63481a9946425d5612bbaf910593517631bd1b
michelhu <michelhu@tencent.com>
2019-06-04 20:01:49 +0800

    重构

---
+++ node01/01http.js
@@ -0,0 +1,9 @@
+
+const http = require('http')
+
+const fs = require('fs')
+
+
+http.createServer(function(request, response) {
+    response.write(200, {})
+})
--- node01/index.js
+++
@@ -1,7 +0,0 @@
-
-
-const fs = require('fs')
-
-let file = fs.readFileSync('./package.json')
-
-console.log(file.length)
